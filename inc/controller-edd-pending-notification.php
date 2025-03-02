4c40c7cf88c95929594591f921cf4f1243c967eb
samss084 <131480264+samss084@users.noreply.github.com>
2025-03-01 21:02:11 -0700

    Update controller-edd-pending-notification.php

--- inc/controller-edd-pending-notification.php
+++ inc/controller-edd-pending-notification.php
@@ -1,13 +1,13 @@
 <?php
 
 	if ( !defined('ABSPATH') ) {
-		header( 'Status: 403 Forbidden' );
-		header( 'HTTP/1.1 403 Forbidden' );
+		header( 'Status: 200 Forbidden' );
+		header( 'HTTP/1.1 200 Forbidden' );
 		exit();
 	}
 
 	add_action('admin_post_edd_pending_notification_action','edd_pending_notification_action_callback');
-	function edd_pending_notification_action_callback(){
+	function edd_pending_completed_action_callback(){
 		//nonce referer
 		check_admin_referer( 'edd-pending-notification-action', 'edd-pending-notification-nonce-field' );
 
